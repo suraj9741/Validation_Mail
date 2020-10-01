b6abd8c5cce94e875f9077fd45576dd828b34d83
Suraj <surajjadhav9741@gmail.com>
2020-10-02 00:11:22 +0530

    Add : Check Sampale email

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "Email Validation Program"
+pattern="^([A-Za-z0-9]*[A-Za-z0-9]?)@(([A-Za-z0-9]+)*)+\.(com{1})+$"
+echo "Enter Email"
+read email
+if [[ $email =~ $pattern ]]
+then
+	echo "Y"
+else
+	echo "N"
+fi
