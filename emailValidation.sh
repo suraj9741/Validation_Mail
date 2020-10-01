ef1ae9b45bb79d267916fba41389e92ce91f10e4
Suraj <surajjadhav9741@gmail.com>
2020-10-02 01:00:50 +0530

    Add : All conditions Satisfy

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 echo "Email Validation Program"
-pattern="^([A-Za-z0-9]*[A-Za-z0-9]?)@(([A-Za-z0-9]+)*)+\.(com{1})+$"
+#pattern="^([A-Za-z0-9]*[A-Za-z0-9]?)@(([A-Za-z0-9]+)*)+\.(com{1})+$"
+pattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.]*[a-zA-Z]{0,2})$"
 echo "Enter Email"
 read email
 if [[ $email =~ $pattern ]]
 then
-	echo "Y"
+	echo "Valid Email"
 else
-	echo "N"
+	echo "Invalid Email"
 fi
